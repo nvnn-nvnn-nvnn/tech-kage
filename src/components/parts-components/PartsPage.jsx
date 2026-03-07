3839b1f40ca46a08e572d6ee6f95a482cdfae894
Devv <korewadevv@gmail.com>
2026-03-07 12:59:30 -0600

    tech fix 3/7 12:59pm

---
+++ src/components/parts-components/PartsPage.jsx
@@ -0,0 +1,21 @@
+import { div } from "framer-motion/client";
+
+export default function PartsPage() {
+
+
+
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+
+        </div>
+
+
+
+    )
+}
